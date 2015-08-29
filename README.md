e04e5b4d2173bd7b95339ffc0f7b6e21124a16fb
胡彬 <franktaylor@163.com>
2015-08-29 17:59:11 +0800

    完善代码

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-�����Ŀ������ѧϰ ��Java 7 Concurrency Cookbook�� ʱ��ͬ���õ����д��롣
-Դ�뱾������һЩ����ģ��Ҹ����Լ���һЩ���⣬�����еĴ������˵�����
+这个项目是我在学习 《Java 7 Concurrency Cookbook》 时，同步敲的书中代码。
+源码本身是有一些问题的，我根据自己的一些理解，对书中的代码做了调整。
--- src/chapter3/Main_7.java
+++ src/chapter3/Main_7.java
@@ -1,5 +1,6 @@
 package chapter3;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Exchanger;
 
@@ -39,9 +40,55 @@ public class Main_7 {
         }
     }
     
+    private static class Consumer implements Runnable {
+    	
+    	private List<String> buffer;
+    	
+    	private final Exchanger<List<String>> exchanger;
+    	
+    	public Consumer(List<String> buffer, Exchanger<List<String>> exchanger) {
+    		this.buffer = buffer;
+    		this.exchanger = exchanger;
+    	}
+    	
+    	@Override
+    	public void run() {
+    		int cycle = 1;
+    		for (int i = 0; i < 10; i++) {
+    			System.out.printf("Consumer: Cycle %d\n", cycle);
+    			
+    			try {
+    				buffer = exchanger.exchange(buffer);
+    			} catch (InterruptedException e) {
+    				e.printStackTrace();
+    			}
+    			
+    			System.out.println("Consumer: " + buffer.size());
+    			for (int j = 0; j < 10; j++) {
+    				String message = buffer.get(0);
+    				System.out.println("Consumer: " + message);
+    				buffer.remove(0);
+    			}
+    			
+    			cycle++;
+    		}
+    	}
+    }
     public static void main(String[] args) {
     	
+    	List<String> buffer1 = new ArrayList<String>();
+    	List<String> buffer2 = new ArrayList<String>();
     	
+    	Exchanger<List<String>> exchanger = new Exchanger<List<String>>();
+    	
+    	Producer producer = new Producer(buffer1, exchanger);
+    	Consumer consumer = new Consumer(buffer2, exchanger);
+    	
+    	Thread threadProducer = new Thread(producer);
+    	Thread threadConsumer = new Thread(consumer);
+    	
+    	threadProducer.start();
+    	threadConsumer.start();
     }
     
     
